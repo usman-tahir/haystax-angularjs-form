fd5c47dde16c5efc2f3f1042dcd35de23b44651a
usman-tahir <tahir.usman.ali94@outlook.com>
2018-01-18 09:59:59 -0500

    feat: refactoring of the registration form; added a phone number regex in the form controller

--- app/js/RegistrationForm.js
+++ app/js/RegistrationForm.js
@@ -5,4 +5,6 @@ app.controller("FormController", ["$scope", function($scope) {
     $scope.email = {
         text: ""
     };
+
+    $scope.phoneNumberRegex = "/^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/im";
 }]);
--- app/js/controller/RegistrationForm.js
+++
@@ -1,8 +0,0 @@
-
-let app = angular.module("RegistrationForm", [])
-
-app.controller("RegistrationFormController", ($scope) => {
-    $scope.email = ""
-    $scope.password = ""
-    $scope.phone = ""
-})
